055025a7f39658193bf966d45dd380af6a0a08f6
Robin Bezak <robin.bezak.99@gmail.com>
2025-06-21 03:48:15 +0200

    prod ready

--- db-init/01_n8n_init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# This script uses psql to execute SQL commands.
-# The shell substitutes $N8N_DB_PASSWORD with the real password before psql runs.
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER n8n_user WITH PASSWORD '$N8N_DB_PASSWORD';
-    CREATE DATABASE n8n_data WITH OWNER n8n_user;
-EOSQL
